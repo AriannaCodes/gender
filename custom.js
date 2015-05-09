f25d182a0c3a85af8615a8861c4fe2dff46ea6ec
AriannaCodes <ariannabenson@gmail.com>
2015-05-09 17:39:35 -0400

    added classes

--- custom.js
+++ custom.js
@@ -1,27 +1,36 @@
-var screens = [{name:"first",
-				text:"Hello, --name--. What are you concentrating in?", 
-				options: [{code:"Click on me!",screen:"second"},
-						  {code:"Other option",screen:"third"},
-						  {code:"This is another statement",screen:"fun"},
-						  {code:"What's going on here",screen:"statistic"}]},
-			   {name:"second",
-			   	text:"This is the second screen. It gives you some options.",
-			   	options:[]},
-			   {name:"third",
-			    text:"This is the third screen. Why did you get here?",
+var screens = [{name:"class_selection",
+				text:"Hello, --name--. So, what do you think you're going to major " +
+					  "in? You haven't declared yet, but your roommate wants to know.", 
+				options: [{code:"Computer Science!",screen:"first_day"},
+						  {code:"Engineering",screen:"first_day"},
+						  {code:"Mathematics",screen:"first_day"},
+						  {code:"Physics",screen:"first_day"},
+						  {code:"Biology",screen:"first_day"},
+						  {code:"English",screen:"end_game"}]},
+			   {name:"first_day",
+			   	text:"It's your first day of classes, and you're going to your " + 
+			   		  "first class, --class--. Where do you sit?",
+			   	options:[{code:"Next to a boy",screen:"boy_convo"},
+			   			 {code:"Next to a girl",screen:"girl_convo"}]},
+			   {name:"boy_convo",
+			    text:"The boy you sit down next to seems pretty cool.",
 			    options:[]},
-			   {name:"fun",
-			    text:"Why won't people on Tinder message me back?",
+			   {name:"girl_convo",
+			    text:"The girl you sit down next to seems pretty cool.",
 			    options:[]},
-			   {name:"statistic",
-			    text:"This is a statistic!",
+			   {name:"end_game",
+			    text:"Well, I hate to say this, but English isn't a science.",
 			    options:[]}];
 
 var player = "Emma";
+var major = 0; // 0 is CS, 1 is Engineering, 2 is Math
+			   // 3 is Physics, 4 is Biology
+var classes = ["CS50", "ES51", "Math 21b", "PS12", "LS1a"];
 
 function genScreen(vals) {
 	nameInserted = vals.text.replace("--name--", player);
-	first =  "<p>" + nameInserted + "</p>";
+	classInserted = nameInserted.replace("--class--", classes[major]);
+	first =  "<p>" + classInserted + "</p>";
 	options = "";
 	for (var i = 0; i < vals.options.length; i++) {
 		options = options + "<a href='#' id='next' screen='" + vals.options[i].screen
@@ -50,4 +59,8 @@ $("#changeable").on("click", "#next", function(e){
 	else {
 		$("#changeable").html(genScreen(screens[pl]));
 	}
+});
+
+$("#refresh").click(function(e){
+	location.reload();
 });
