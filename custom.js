77698b7ec27707ea25fb2dde3e20a458c99563c3
AriannaCodes <ariannabenson@gmail.com>
2015-05-10 12:54:56 -0400

    added some more options

--- custom.js
+++ custom.js
@@ -1,52 +1,161 @@
+/* Various screens in the game */
 var screens = [{name:"class_selection",
 				text:"Hello, --name--. So, what do you think you're going to major " +
 					  "in? You haven't declared yet, but your roommate wants to know.", 
-				options: [{code:"Computer Science!",screen:"first_day"},
-						  {code:"Engineering",screen:"first_day"},
-						  {code:"Mathematics",screen:"first_day"},
-						  {code:"Physics",screen:"first_day"},
-						  {code:"Biology",screen:"first_day"},
-						  {code:"English",screen:"end_game"}]},
+				options: [{code:"Computer Science",type:"classes",screen:"0"},
+						  {code:"Engineering",type:"classes",screen:"1"},
+						  {code:"Mathematics",type:"classes",screen:"2"},
+						  {code:"Physics",type:"classes",screen:"3"},
+						  {code:"Biology",type:"classes",screen:"4"},
+						  {code:"English",type:"classes",screen:"english_major"}],
+				source: ""},
+
 			   {name:"first_day",
 			   	text:"It's your first day of classes, and you're going to your " + 
-			   		  "first class, --class--. Where do you sit?",
-			   	options:[{code:"Next to a boy",screen:"boy_convo"},
-			   			 {code:"Next to a girl",screen:"girl_convo"}]},
-			   {name:"boy_convo",
+			   		  "9am class, --class--. Where do you sit?",
+			   	options:[{code:"Next to a boy",type:"next",screen:"boy_convo"},
+			   			 {code:"Next to a girl",type:"next",screen:"girl_convo"}],
+			   	source: ""},
+
+			   {name:"english_major",
+			    text:"Well, I hate to say this, but English isn't a science.",
+			    options:[],
+			    source: ""},
+
+			    {name:"boy_convo",
 			    text:"The boy you sit down next to seems pretty cool.",
-			    options:[]},
+			    options:[],
+			    source: ""},
+
 			   {name:"girl_convo",
 			    text:"The girl you sit down next to seems pretty cool.",
-			    options:[]},
-			   {name:"end_game",
-			    text:"Well, I hate to say this, but English isn't a science.",
-			    options:[]}];
+			    options:[],
+			    source: ""},
 
+			    {name:"major_percentages",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"friday",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"go_out",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"stay_in",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"unlocking",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"office_hours",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"midterm",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"classroom",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"boy_convo2",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"girl_convo2",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"attrition",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"internship",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+				{name:"final_project",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"hackathon",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"lab_work",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"secretary",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"winter_break",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"good_odds",
+			    text:"...",
+			    options:[],
+			    source: ""},
+
+			    {name:"second_semester",
+			    text:"...",
+			    options:[],
+			    source: ""}];
+
+/* Info about the player, with default name */
 var player = "Emma";
 var major = 0; // 0 is CS, 1 is Engineering, 2 is Math
 			   // 3 is Physics, 4 is Biology
 var classes = ["CS50", "ES51", "Math 21b", "PS12", "LS1a"];
 
+/* Generates the screen requested */
 function genScreen(vals) {
 	nameInserted = vals.text.replace("--name--", player);
 	classInserted = nameInserted.replace("--class--", classes[major]);
 	first =  "<p>" + classInserted + "</p>";
 	options = "";
 	for (var i = 0; i < vals.options.length; i++) {
-		options = options + "<a href='#' id='next' screen='" + vals.options[i].screen
-				  + "' > >>> " + vals.options[i].code + "</a><br />";
+		options = options + "<a href='#' id='" + vals.options[i].type + 
+							 "' screen='" + vals.options[i].screen +
+							 "' > >>> " + vals.options[i].code + 
+						     "</a><br />";
 	}
-	return first + options;
+	if (vals.source != "") {
+		source = "<div id='source'>(" + vals.source +")</div>";
+	}
+	else {
+		source = "";
+	}
+	return first + options + source;
 }
 
-$("#start").click(function(e){
-	player = $("#player").val();
-	$("#changeable").html(genScreen(screens[0]));
-});
-
-$("#changeable").on("click", "#next", function(e){
-	// iterate to find correct screen
-	var q = $(this).attr("screen");
+/* Iterates through screens to find correct one */
+function changeTo(q) {
 	var pl = -1;
 	for (var i = 0; i < screens.length; i++) {
 		if (screens[i].name == q) {
@@ -59,8 +168,32 @@ $("#changeable").on("click", "#next", function(e){
 	else {
 		$("#changeable").html(genScreen(screens[pl]));
 	}
+}
+
+/* Different actions for different pages */
+$("#start").click(function(e){
+	if ($("#player").val() != "") {
+		player = $("#player").val();
+	}
+	changeTo("class_selection");
 });
 
+$("#changeable").on("click", "#classes", function(e){
+	if ($(this).attr("screen") != "english_major") {
+		major = parseInt($(this).attr("screen"));
+		changeTo("first_day");
+	}
+	else {
+		changeTo("end_game");
+	}
+});
+
+$("#changeable").on("click", "#next", function(e){
+	var q = $(this).attr("screen");
+	changeTo(q);
+});
+
+/* Refreshes the page! */
 $("#refresh").click(function(e){
 	location.reload();
 });
